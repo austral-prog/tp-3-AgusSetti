c0bd7614120ef368680434ea73dfb30b5604a2f3
AgusSetti <asetti@mail.austral.edu.ar>
2025-04-07 14:59:28 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,6 @@
 def check_vowels():
     
-   nombre= input("Ingrese su nombre: ")
+    nombre= input("Ingrese su nombre: ")
     print(f"Contiene a:", "a" in nombre.lower())
     print(f"Contiene e:", "e" in nombre.lower())
     print(f"Contiene i:", "i" in nombre.lower())
